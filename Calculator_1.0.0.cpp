0af7682b70551ce02c5658eab1177bb241c8f072
omerfaruk1234 <57019559+omerfaruk1234@users.noreply.github.com>
2024-05-05 19:08:34 +0200

    Update Calculator_1.5.0.cpp
    I just added multiplication and division functions to my code, for this you need to type 3 or 4 into the terminal. Next update: I want to add a while loop, the calculator will ask if it should be closed when it is finished working...

--- Calculator_1.0.0.cpp
+++ Calculator_1.0.0.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
     int number;
-    float num1, num2, addition, subtraction;
+    double num1, num2, addition, subtraction, multiplication, division;
     bool checkerForFirstInput, checkerForSecondInput;
 
     cout<< "Welcome to the Calculator!\n"
-        << "Enter 1 for addition or 2 for subtraction: ";
+        << "Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division: ";
     cin >> number;
     do {
         if (cin.fail()) {
@@ -17,11 +17,11 @@ int main(){
             cin >> number;
             
         }
-        else if ((number != 1 && number != 2)){
+        else if ((number != 1 && number != 2 && number != 3 && number!=4)){
             cout << "Please enter right number ";
             cin >> number;
         }
-    } while ((number != 1 && number != 2));
+    } while ((number != 1 && number != 2 && number != 3 && number!=4));
     
     switch (number){
         case 1:
@@ -93,6 +93,77 @@ int main(){
             subtraction=(num1-num2);
             cout << "The solution of subtraction is: " << subtraction;            
             break;
-            }
+            
+        case 3:
+            cout << "Please type the first number to multiplication: ";
+            cin >> num1;
+            checkerForFirstInput = cin.fail();
+            do
+            {
+                if (checkerForFirstInput==1){
+                    cout << "Please enter a valid number for first number: ";
+                    cin.clear(); 
+                    cin.ignore(1000, '\n');
+                    cin >> num1;
+                    checkerForFirstInput = cin.fail();
+                }
+            } while (checkerForFirstInput==1);
+            
+            cout << "Please type the second number to multiplication: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cin >> num2;
+            checkerForSecondInput = cin.fail();
+            do
+            {
+                if (checkerForSecondInput==1){
+                    cout << "Please enter a valid number for second number: ";
+                    cin.clear(); 
+                    cin.ignore(1000, '\n');
+                    cin >> num2;
+                    checkerForSecondInput = cin.fail();
+                }
+
+            } while (checkerForSecondInput==1);
+            multiplication=(num1*num2);
+            cout << "The solution of multiplication is: " << multiplication;
+            break;
+                    
+        case 4:
+            cout << "Please type the first number to division: ";
+            cin >> num1;
+            checkerForFirstInput = cin.fail();
+            do
+            {
+                if (checkerForFirstInput==1){
+                    cout << "Please enter a valid number for first number: ";
+                    cin.clear(); 
+                    cin.ignore(1000, '\n');
+                    cin >> num1;
+                    checkerForFirstInput = cin.fail();
+                }
+            } while (checkerForFirstInput==1);
+            
+            cout << "Please type the second number to division: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cin >> num2;
+            checkerForSecondInput = cin.fail();
+            do
+            {
+                if (checkerForSecondInput==1){
+                    cout << "Please enter a valid number for second number: ";
+                    cin.clear(); 
+                    cin.ignore(1000, '\n');
+                    cin >> num2;
+                    checkerForSecondInput = cin.fail();
+                }
+
+            } while (checkerForSecondInput==1);
+            division=(num1/num2);
+            cout << "The solution of division is: " << division;
+            break;
+     
+        }
 return 0;
 }
